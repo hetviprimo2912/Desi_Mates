905633600863662c9bc20f987c739311b6d1af50
rohan-Amin12 <rohan.primocys@example.com>
2026-07-14 10:48:28 +0530

    fixed build

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from "./Store/store";
 createRoot(document.getElementById("root")!).render(
 
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
       <App />
     </BrowserRouter>
   </Provider>
